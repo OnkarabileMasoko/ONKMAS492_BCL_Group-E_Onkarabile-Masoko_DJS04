ccaa4a593534ee35d021745cdf332f376a091f34
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-05-17 16:44:10 +0200

    added a detail.js file

--- ONKMAS-492_BCL2024_E_Onkarabile-Masoko_DJS03-main/ONKMAS-492_BCL2024_E_Onkarabile-Masoko_DJS03-main/book-preview.js
+++ ONKMAS-492_BCL2024_E_Onkarabile-Masoko_DJS03-main/ONKMAS-492_BCL2024_E_Onkarabile-Masoko_DJS03-main/book-preview.js
@@ -4,79 +4,38 @@ class BookPreview extends HTMLElement {
       this.attachShadow({ mode: 'open' });
     }
   
-    static get observedAttributes() {
-      return ['author', 'id', 'image', 'title'];
-    }
-  
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (oldValue !== newValue) {
-        this.render();
-      }
-    }
-  
     connectedCallback() {
-      this.render();
-    }
+      const author = this.getAttribute('author');
+      const id = this.getAttribute('data-preview');
+      const image = this.getAttribute('image');
+      const title = this.getAttribute('title');
   
-    render() {
-      const { author, id, image, title } = this.dataset;
       this.shadowRoot.innerHTML = `
         <style>
           .preview {
-            border-width: 0;
-            width: 100%;
-            font-family: Roboto, sans-serif;
-            padding: 0.5rem 1rem;
             display: flex;
-            align-items: center;
+            flex-direction: column;
+            border: 1px solid #ccc;
+            padding: 10px;
             cursor: pointer;
-            text-align: left;
-            border-radius: 8px;
-            border: 1px solid rgba(var(--color-dark), 0.15);
-            background: rgba(var(--color-light), 1);
           }
-  
-          @media (min-width: 60rem) {
-            .preview {
-              padding: 1rem;
-            }
-          }
-  
-          .preview:hover {
-            background: rgba(var(--color-blue), 0.05);
-          }
-  
           .preview__image {
-            width: 48px;
-            height: 70px;
-            object-fit: cover;
-            background: grey;
-            border-radius: 2px;
-            box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-                        0px 1px 1px 0px rgba(0, 0, 0, 0.1),
-                        0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+            width: 100%;
+            height: auto;
           }
-  
           .preview__info {
-            padding: 1rem;
+            margin-top: 10px;
           }
-  
           .preview__title {
-            margin: 0 0 0.5rem;
-            font-weight: bold;
-            display: -webkit-box;
-            -webkit-line-clamp: 2;
-            -webkit-box-orient: vertical;
-            overflow: hidden;
-            color: rgba(var(--color-dark), 0.8);
+            font-size: 1.2em;
+            margin: 0;
           }
-  
           .preview__author {
-            color: rgba(var(--color-dark), 0.4);
+            color: #555;
           }
         </style>
         <button class="preview" data-preview="${id}">
-          <img class="preview__image" src="${image}" alt="Book cover" />
+          <img class="preview__image" src="${image}" />
           <div class="preview__info">
             <h3 class="preview__title">${title}</h3>
             <div class="preview__author">${author}</div>
